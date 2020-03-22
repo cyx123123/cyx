6591c4fd5a6e5b459e8bf56cee9fff8db17fbb27
LAPTOP-DGTFV118\天然呆 <1741299324@qq.com>
2020-03-22 10:31:10 +0800

    马奇凯蓝湖移动端

---
+++ 马奇凯蓝湖作品/js/listData.js
@@ -0,0 +1,77 @@
+// 模拟后台数据----假数据
+// 前端访问不到mysql数据库
+// 前端只能利用ajax跟后台通讯
+
+var mesa = {
+  data: [
+    {
+      logo: "img/凯莱德logo@2x.png",
+      word:
+        "候选人为陈万先的委托,由CBC信用管理有限公司核查完成，请进入委托界面查看详情",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "1"
+    },
+    {
+      logo: "img/美食客logo@2x.png",
+      word:
+        "候选人为陈万先的委托,由CBC信用管理有限公司核查完成，请进入委托界面查看详情",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "0"
+    },
+    {
+      logo: "img/360logi@2x.png",
+      word: "有一份新的委托需要您帮助协查",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "1"
+    },
+    {
+      logo: "img/凯莱德logo@2x.png",
+      word:
+        "候选人为陈万先的委托,由CBC信用管理有限公司核查完成，请进入委托界面查看详情",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "0"
+    },
+    {
+      logo: "img/美食客logo@2x.png",
+      word: "有一份新的委托需要您帮助协查",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "1"
+    },
+    {
+      logo: "img/凯莱德logo@2x.png",
+      word:
+        "候选人为陈万先的委托,由CBC信用管理有限公司核查完成，请进入委托界面查看详情",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "0"
+    },
+    {
+      logo: "img/美食客logo@2x.png",
+      word: "有一份新的委托需要您帮助协查",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "1"
+    },
+    {
+      logo: "img/凯莱德logo@2x.png",
+      word:
+        "候选人为陈万先的委托,由CBC信用管理有限公司核查完成，请进入委托界面查看详情",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "0"
+    },
+    {
+      logo: "img/360logi@2x.png",
+      word:
+        "候选人为陈万先的委托,由CBC信用管理有限公司核查完成，请进入委托界面查看详情",
+      subTimeb: "14:20",
+      subTime: "2018-03-29",
+      state: "1"
+    }
+  ]
+};
